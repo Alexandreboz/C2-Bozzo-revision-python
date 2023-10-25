4b4e72d231968621adcb3a46587a71e0bd538d2b
alexandreboz <bozzomvp@gmail.com>
2023-10-25 11:09:14 +0200

    exercice4

---
+++ exercice4.py
@@ -0,0 +1,9 @@
+liste_nombre = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+maximum = max(liste_nombre)
+minimum = min(liste_nombre)
+moyenne = sum(liste_nombre)/len(liste_nombre)
+
+print("Le nombre maximum est : ",maximum)
+print("Le nombre minimum est : ",minimum)
+print("La moyenne est : ",moyenne)
