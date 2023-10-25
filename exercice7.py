78ef719c89f692028e7fbdda3db2c215c31e07e7
alexandreboz <bozzomvp@gmail.com>
2023-10-25 12:15:06 +0200

    Exercice 7 v2

--- exercice7.py
+++ exercice7.py
@@ -6,7 +6,7 @@ longueur = len(nombre_mots)
 longueurstr = str(longueur)
 
 fichier2= open("fichier2.txt","a")
-fichier2.write(longueurstr)
+fichier2.write("Le nombre de mots du fichier 1 est : "+ longueurstr)
 
 fichier1.close()
 fichier2.close()
